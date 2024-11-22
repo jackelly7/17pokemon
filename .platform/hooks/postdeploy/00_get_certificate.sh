f9f0bbb091eff6d20d5d7ddb80250f3661df777e
jackelly7 <johnmcmillankelly@gmail.com>
2024-11-21 20:30:22 -0700

    group 1-7 is back

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d http://pokemon1-7.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email jackell2@byu.edu
+sudo certbot -n -d pokemon1-7.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email jackell2@byu.edu
