f850592c8bd2ee06357d6c649b2d5e0d6afbcdfd
jackelly7 <johnmcmillankelly@gmail.com>
2024-11-21 20:59:06 -0700

    were tired

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d pokemon1-7.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email jackell2@byu.edu
+sudo certbot -n -d 17pokemon.is404.net --nginx --agree-tos --email jackell2@byu.edu
